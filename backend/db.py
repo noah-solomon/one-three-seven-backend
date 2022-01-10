50b7cbcf02c0d4e588777b092e61c507afc15390
Noah Solomon <njs99@cornell.edu>
2022-01-10 16:54:37 -0700

    Updated Dockerfile

--- backend/db.py
+++ backend/db.py
@@ -9,16 +9,17 @@ class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
     done = db.Column(db.Boolean, default=False)
-    create_date = db.Column(db.DateTime, default=datetime.now)
+    create_date = db.Column(db.DateTime, default=None)
 
     def __init__(self, **kwargs):
-        self.code = kwargs.get('code', '')
         self.title = kwargs.get('title', '')
         self.done = kwargs.get('done', False)
+        self.create_date = datetime.now()
 
     def serialize(self):
         return {
             'id': self.id,
             'title': self.title,
-            'done': self.done
+            'done': self.done,
+            'create_date': str(self.create_date) if self.create_date else None
         }
