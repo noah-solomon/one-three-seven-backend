b335ca2a22cc7e7ff940cf4aa0c5fc311575d649
Noah Solomon <njs99@cornell.edu>
2022-01-10 22:23:48 -0700

    Fix flask again

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
-DATABASE_URL=$(heroku config:get DATABASE_URL -a ots-tasks-backend) flask db init
+echo "Database Url:"
+heroku config:get DATABASE_URL -a ots-tasks-backend
+flask db init
 flask db migrate
 flask db upgrade
 gunicorn app:app
