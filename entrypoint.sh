500e2fad0ed968b0dec85f308afce88f55ca0888
Noah Solomon <njs99@cornell.edu>
2022-01-10 22:34:16 -0700

    FINAL FIX

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 heroku pg:wait -a ots-tasks-backend
-echo "Database Url:"
-heroku config:get DATABASE_URL -a ots-tasks-backend
-flask db init
 flask db migrate
 flask db upgrade
 gunicorn app:app
